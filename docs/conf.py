2d6eeaf38619cc1aab87309f34805d15b87e4588
danilosauro <danilodias_lc@hotmail.com>
2025-09-29 14:04:27 -0300

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,13 @@
+import os, sys
+sys.path.insert(0, os.path.abspath("..")) 
 
-extensions = ["myst_parser"]
-myst_enable_extensions = ["colon_fence", "deflist", "fieldlist", "tasklist", "linkify"]
+project = "CIDACS-PHDC"
+extensions = [
+    "myst_parser",         
+]
+myst_enable_extensions = ["colon_fence", "deflist", "fieldlist", "tasklist"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
--- docs/index.md
+++ docs/index.md
@@ -1,24 +1,8 @@
 # CIDACS-PHDC
 
+Welcome, use the menu to navigate!
+
 ```{toctree}
 :maxdepth: 2
-readme
-license
-pdfs
-scripts
-
-
-Sphinx (MyST) permite **includes** nativos:
-
-**`docs/readme.md`**
-```md
-# Visão geral
-```{include} ../README.md 
-
-
-
-**`docs/license.md`**
-```md
-# Licença
-```{include} ../LICENSE
-
+PDFs
+Scripts
